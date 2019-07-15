57a8fe556fbb4de60ed781e471a749da052df126
Adam Chehadi <adampchehadi@gmail.com>
2019-07-15 00:55:01 -0600

    Created Player model, maniplutaed button enable property

---
+++ Swoosh/Swoosh/Model/player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  Swoosh
+//
+//  Created by Adam Chehadi on 7/15/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
