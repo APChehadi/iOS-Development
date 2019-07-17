9b5d8c5c8273d290b64fc79a26618f7fa13b1002
Adam Chehadi <adampchehadi@gmail.com>
2019-07-16 22:58:20 -0600

    Custom text fields

---
+++ window-shopper/window-shopper/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Adam Chehadi on 7/16/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
