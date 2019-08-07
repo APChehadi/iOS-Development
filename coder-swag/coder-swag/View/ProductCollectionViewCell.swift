c67bb0896a5d18bddcad094321cc2395e4b74c85
Adam Chehadi <adampchehadi@gmail.com>
2019-08-07 00:53:29 -0600

    UICollectionView data model

---
+++ coder-swag/coder-swag/View/ProductCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Adam Chehadi on 8/7/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
