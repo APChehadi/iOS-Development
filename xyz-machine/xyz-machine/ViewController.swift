ed900004fbc22d8d2d5b612ee8c474b0d20b0ef8
Adam Chehadi <adampchehadi@gmail.com>
2019-08-07 23:41:07 -0600

    Create XYZ project, labels, IBOutlets

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Adam Chehadi on 8/7/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
