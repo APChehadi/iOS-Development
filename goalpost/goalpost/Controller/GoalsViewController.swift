9688b949714ad240743c8058a535cc347a8433f8
Adam Chehadi <adampchehadi@gmail.com>
2019-08-11 13:38:16 -0600

    Saving data to persistent store

--- goalpost/goalpost/Controller/GoalsViewController.swift
+++ goalpost/goalpost/Controller/GoalsViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class GoalsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
