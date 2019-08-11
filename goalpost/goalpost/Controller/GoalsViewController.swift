b47a8863e430d379c249564259c4117edd0f99d9
Adam Chehadi <adampchehadi@gmail.com>
2019-08-11 14:41:21 -0600

    Removing objects from persistent store

--- goalpost/goalpost/Controller/GoalsViewController.swift
+++ goalpost/goalpost/Controller/GoalsViewController.swift
@@ -27,6 +27,11 @@ class GoalsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        fetchCoreDataObjects()
+        tableView.reloadData()
+    }
+    
+    func fetchCoreDataObjects() {
         self.fetch { (complete) in
             if goals.count >= 1 {
                 tableView.isHidden = false
@@ -34,7 +39,6 @@ class GoalsViewController: UIViewController, UITableViewDelegate, UITableViewDat
                 tableView.isHidden = true
             }
         }
-        tableView.reloadData()
     }
 
     @IBAction func addGoalBtnWassPressed(_ sender: Any) {
@@ -55,6 +59,17 @@ class GoalsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
     }
     
+    func removeGoal(atIndexPath indexPath: IndexPath) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        managedContext.delete(goals[indexPath.row])
+        do {
+            try managedContext.save()
+            print("Successfully removed goal!")
+        } catch {
+            debugPrint("Could not remove: \(error.localizedDescription)")
+        }
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -68,6 +83,24 @@ class GoalsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         let goal = goals[indexPath.row]
         cell.configureCell(goal: goal)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return UITableViewCell.EditingStyle.delete
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            self.removeGoal(atIndexPath: indexPath)
+            self.fetchCoreDataObjects()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        deleteAction.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        return [deleteAction]
     }
     
 }
