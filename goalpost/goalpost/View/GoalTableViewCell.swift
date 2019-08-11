3e12974ec6090174aef9873647d74284e2828267
Adam Chehadi <adampchehadi@gmail.com>
2019-08-10 21:21:46 -0600

    UITableViewCell subclass

---
+++ goalpost/goalpost/View/GoalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalTableViewCell.swift
+//  goalpost
+//
+//  Created by Adam Chehadi on 8/10/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import UIKit
+
+class GoalTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        goalDescriptionLbl.text = description
+        goalTypeLbl.text = type
+        goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+
+}
