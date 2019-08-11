abdaac856cfe284c7c5af4e8481d3fa2c26f49c3
Adam Chehadi <adampchehadi@gmail.com>
2019-08-10 23:56:54 -0600

    Create UIView and UIButton extension

--- goalpost/goalpost/Controller/CreateGoalViewController.swift
+++ goalpost/goalpost/Controller/CreateGoalViewController.swift
@@ -14,17 +14,26 @@ class CreateGoalViewController: UIViewController {
     @IBOutlet weak var shortTermBtn: UIButton!
     @IBOutlet weak var longTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
+    
+    var goalType: GoalType = .shortTerm
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nextBtn.bindToKeyboard()
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     
     @IBAction func longTermBtnWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        shortTermBtn.setDeselectedColor()
+        longTermBtn.setSelectedColor()
     }
     
     @IBAction func nextBtnWasPressed(_ sender: Any) {
