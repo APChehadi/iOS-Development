1edf8a18f60f5684fb46b0f5f055000b95a92610
Adam Chehadi <adampchehadi@gmail.com>
2019-08-10 20:49:06 -0600

    Created goalpost project, custom ViewController with UITableView and IBOutlets

---
+++ goalpost/goalpost/Controller/GoalsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsViewController.swift
+//  goalpost
+//
+//  Created by Adam Chehadi on 8/10/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import UIKit
+
+class GoalsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addGoalBtnWassPressed(_ sender: Any) {
+        print("Button was pressed")
+    }
+    
+}
+
