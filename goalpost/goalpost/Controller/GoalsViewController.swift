569c4e4e685d343a2592deaef5fa3565438bd26e
Adam Chehadi <adampchehadi@gmail.com>
2019-08-10 22:36:37 -0600

    Creating core data entity & attributes, displaying static data in UITableViewCells

--- goalpost/goalpost/Controller/GoalsViewController.swift
+++ goalpost/goalpost/Controller/GoalsViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class GoalsViewController: UIViewController {
+class GoalsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
 
     @IBAction func addGoalBtnWassPressed(_ sender: Any) {
         print("Button was pressed")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalTableViewCell") as? GoalTableViewCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week.", type: .shortTerm, goalProgressAmount: 2)
+        return cell
     }
     
 }
---
+++ goalpost/goalpost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost
+//
+//  Created by Adam Chehadi on 8/10/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
